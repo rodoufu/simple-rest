d8ccf3494c01d4ce8e64671d4edcd547e624800d
Rodolfo Araujo <rodoufu@gmail.com>
2020-02-09 12:47:17 -0300

    Adding connexion and swagger-ui

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
-from flask import (
-	Flask,
-	render_template
-)
+from flask import render_template
+import connexion
 
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
 
 
 @app.route('/')
@@ -16,4 +15,4 @@ This function just responds to the browser URL localhots:5000
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
